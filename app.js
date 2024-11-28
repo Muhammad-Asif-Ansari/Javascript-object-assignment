ce71fb8da5b863d439f8c224db26e8fc255596bb
Muhammad-Asif-Ansari <muhammadasifansari101@gmail.com>
2024-11-28 21:33:38 +0500

    Object & Constructor
    Object & Constructor

---
+++ app.js
@@ -0,0 +1,98 @@
+// Question no 01:
+
+
+// var itemsArray = [
+//     {
+//         name:"juice",
+//         price:50,
+//         quantity:3,
+//     },
+//     {
+//         name:"cookie",
+//         price:30,
+//         quantity:3,
+//     },
+//     {
+//         name:"shirt",
+//         price:880,
+//         quantity:1,
+//     },
+//     {
+//         name:"pen",
+//         price:100,
+//         quantity:2,
+//     }
+// ]
+// var totalSum = 0;
+// for(var i = 0; i < itemsArray.length; i++){
+//     console.log(itemsArray[i].price * itemsArray[i].quantity);
+//     totalSum += itemsArray[i].price * itemsArray[i].quantity;
+// }
+
+// console.log("The total price of all items", totalSum);
+
+
+
+// Question no 02:
+// var student = {
+//     name:"asif",
+//     email:"muhammadasifansari101@gmail.com",
+//     password:123456,
+//     age:21,
+//     gender:"male",
+//     city:"Karachi",
+//     country:"Pakistan"
+// }
+
+// console.log("age" in student);
+// console.log("country" in student);
+
+// console.log("firstName" in student);
+// console.log("lastName" in student);
+
+
+
+
+// Question n0 03:
+// function User(name,age,gender,city){
+//     this.name = name;
+//     this.age = age;
+//     this.gender = gender;
+//     this.city = city;
+// }
+
+// var user1 = new User ("asif",21,"male","karachi")
+// var user2 = new User("mateen",15,"male","lahore")
+// var user3 = new User ("mubeen",19,"male","multan")
+// var user4 = new User ("tahir",24,"male","karachi")
+
+// console.log(user1);
+// console.log(user2);
+// console.log(user3);
+// console.log(user4);
+
+
+//Question no 04:
+
+// function UserData(name,gender,address,education,profession){
+//     this.name = name;
+//     this.gender = gender;
+//     this.address = address;
+//     this.education = education;
+//     this.profession = profession;
+// }
+
+// var userData1 = new User ("asif","male","Majeed Colony","Inter","Developer")
+// var userData2 = new User ("Hafeez","male","Landhi","Masters","Doctor")
+// var userData3 = new User ("Amir","male","Malir","Masters","Civil Engineer")
+// var userData4 = new User ("Kamran","male","Numaish","Graduate","Mechanical Engineer")
+
+// console.log(userData1);
+// console.log(userData2);
+// console.log(userData3);
+// console.log(userData4);
+
+
+
+
+
